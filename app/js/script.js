b1a70d3e759c64c58ea876ff9f3d4174aed6202c
Andrew Soldini <ASoldini@Andrews-MacBook-Pro-38.local>
2018-01-19 13:34:49 -0500

    almost

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,8 @@
 const form = document.forms[0]
 var them = []
 var assigned = false
+var id
+var night = false
 
 var players = [{name: "he", isAlive: true, isDetective: false, isMafia: false, isDoctor: false},
 {name: "he", isAlive: true, isDetective: false, isMafia: false, isDoctor: false},
@@ -13,29 +15,22 @@ var players = [{name: "he", isAlive: true, isDetective: false, isMafia: false, i
 {name: "he", isAlive: true, isDetective: false, isMafia: false, isDoctor: false},
 {name: "he", isAlive: true, isDetective: false, isMafia: false, isDoctor: false}]
 
+var inProgress = false
+var socket = null
+
 function startGame() {
-	// socket.on("start", function(players) {
-	// 	localStorage.players = JSON.stringify(players)
-	// 	localStorage.night = "true"
-	console.log("game stated")
-	makeYou()
-	makeThem()
-
-	// })
-}
-
-
-function register2(){
 	//Opening code with setup for getting ids and access
-	socket = io.connect("http://localhost:5000")
+	if (!inProgress) {
+		socket = io.connect("http://localhost:5000")
+		inProgress = true	
+	}
 	socket.on("id", function (data) {
 		console.log("id received")
 		if(!assigned){
 			id = data;
-			var nick = form.nickname.value
-			localStorage.id = id
+			var nick = 'Noah'
 			assigned = true;
-			socket.emit("name", {name: nick})
+			socket.emit("name", {nick: nick, id: id})
 			// socket.on(id.toString(), function (data){
 			// 	if(data == "mafia"){
 			// 		isMafia = true
@@ -48,55 +43,62 @@ function register2(){
 			// 	}
 			// })
 			console.log(id)
-			window.location = '/room'
-			startGame()
+		makeYou()
+		console.log("made them")
+		makeThem()
 		}
-	socket.on("update", function(players) {
-				localStorage.players = JSON.stringify(players)
-				makeYou()
-				makeThem()
-				if (localStorage.night === "true") {
-					localStorage.night = "false"
-					toDay()
+		socket.on("update", function(plays) {
+			players = plays
+			makeYou()
+			makeThem()
+				if (night === false) {
+					toNight()
+					night = true
 				}
 				else{
-					localStorage.night = "true"
-					toNight()
+					toDay()
+					night = false
 				}
-			})	
+		})	
 	})
 
+
+}
+
+
+function register2(){
+	window.location = '/room'
 }
 
 function toDay() {
 	document.body.style.backgroundColor = "teal"
+	document.getElementById("task").innerHTML = "Choose Player to Lynch"
 	voteOn()
 }
 
 function toNight() {
 	document.body.style.backgroundColor = "black"
-	players = JSON.parse(localStorage.players)
-	if (players[localStorage.id.parseInt()].isDetective) {
-		alert("Choose Player to Reveal")
+	if (players[id].isDetective) {
+		document.getElementById("task").innerHTML = "Choose Player to Reveal" 
 		detectOn()
 	}
-	else if (players[localStorage.id.parseInt()].isMafia) {
-		alert("Choose Player to Kill")
+	else if (players[id].isMafia) {
+		document.getElementById("task").innerHTML = "Choose Player to Kill"
 		murderOn()
 	}
-	else if (players[localStorage.id.parseInt()].isDoctor) {
-		alert("Choose Player to Save")
+	else if (players[id].isDoctor) {
+		document.getElementById("task").innerHTML = "Choose Player to Save"
 		healOn()
 	}
 	else {
-		alert("Wait for Night to End")
+		document.getElementById("task").innerHTML = "Wait for Night to End"
 	}
 	
 }
 
 function makeYou() {
 	//players = JSON.parse(localStorage.players)
-	var currentUser = players[localStorage.id.parseInt()]
+	var currentUser = players[id]
 	if (currentUser.isAlive === false) {
 		document.getElementById("your_icon_img").src = '/img/dead.png'
 	}
@@ -117,19 +119,19 @@ function makeYou() {
 
 function makeThem() {
 	//players = JSON.parse(localStorage.players)
-	them = JSON.parse(players).slice()
-	them.splice(localStorage.id.parseInt(),1)
+	them = players.slice()
+	them.splice(id,1)
 	for (var l = 0; l < 9; l++) {
-		if (them[i].isAlive === false) {
-			document.getElementById("icon_user_" + (j+2).toString()).src = '/img/dead.png'
+		if (them[l].isAlive === false) {
+			document.getElementById("icon_user_" + (l+2).toString()).src = '/img/dead.png'
 		}
-		else if (players[localStorage.id.parseInt()].isMafia === true && them[i].isMafia === true) {
-			document.getElementById("icon_user_" + (j+2).toString()).src = '/img/mafia.png'
+		else if (players[id].isMafia === true && them[l].isMafia === true) {
+			document.getElementById("icon_user_" + (l+2).toString()).src = '/img/mafia.png'
 		}
 		else {
-			document.getElementById("icon_user_" + (j+2).toString()).src = '/img/innocent.png'
+			document.getElementById("icon_user_" + (l+2).toString()).src = '/img/innocent.png'
 		} 
-		document.getElementById("name_user_" + (j+2).toString()).innerHTML = them[i].name
+		document.getElementById("name_user_" + (l+2).toString()).innerHTML = them[l].name
 	}
 }
 
@@ -139,32 +141,33 @@ function hello() {
 
 function detectOn() {
 	for (var j = 0; j < 9; j++) {
-		if (them[i].isAlive === true) {
+		if (them[j].isAlive === true) {
 			document.getElementById("icon_user_" + (j+2).toString()).onclick = detect.bind(null, j)
 		}
 	}
 }
 
 function voteOn() {
-	for (var j = 0; j < 9; j++) {
-		if (them[i].isAlive === true) {
-			document.getElementById("icon_user_" + (j+2).toString()).onclick = vote.bind(null, j)
+	console.log("voting on")
+	for (var x = 0; x < 9; x++) {
+		if (them[x].isAlive === true) {
+			document.getElementById("icon_user_" + (x+2).toString()).onclick = vote.bind(null, x)
 		}
 	}
 }
 
 function murderOn() {
-	for (var j = 0; j < 9; j++) {
-		if (them[i].isAlive === true) {
-			document.getElementById("icon_user_" + (j+2).toString()).onclick = murder.bind(null, j)
+	for (var y = 0; y < 9; y++) {
+		if (them[y].isAlive === true) {
+			document.getElementById("icon_user_" + (y+2).toString()).onclick = murder.bind(null, y)
 		}
 	}
 }
 
 function healOn() {
-	for (var j = 0; j < 9; j++) {
-		if (them[i].isAlive === true) {
-			document.getElementById("icon_user_" + (j+2).toString()).onclick = heal.bind(null, j)
+	for (var z = 0; z < 9; z++) {
+		if (them[z].isAlive === true) {
+			document.getElementById("icon_user_" + (z+2).toString()).onclick = heal.bind(null, z)
 		}
 	}
 }
@@ -186,20 +189,22 @@ function detect(index) {
 }
 
 function vote(index) {
+	socket.emit("dayVote", {id: index})
 	for (var k = 1; k < 10; k++) {
 		document.getElementById("icon_user_" + (k+1).toString()).onclick = null
 	}
 }
 
 function murder(index) {
-	
+	console.log("Tried to kill")
+	socket.emit("mafiakill", {id: index})
 	for (var k = 1; k < 10; k++) {
 		document.getElementById("icon_user_" + (k+1).toString()).onclick = null
 	}	
 }
 
 function heal(index) {
-	
+	socket.emit("heal", {id: index})
 	for (var k = 1; k < 10; k++) {
 		document.getElementById("icon_user_" + (k+1).toString()).onclick = null
 	}	
